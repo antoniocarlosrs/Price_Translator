9e671e457b168e442df6190c76465862e7e82ee6
antoniocarlosrs <antoniocarlosrs.dev@gmail.com>
2024-12-10 22:39:33 -0300

    Finalização do gráfico

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const searchForm = document.querySelector(".search-form");
 const productList = document.querySelector('.product_list')
-const priceChart = document.querySelector('price-chart')
+const priceChart = document.querySelector('.price-chart')
 
 //Variável para guardar os gráficos
 let myChart = ''
@@ -35,10 +35,55 @@ function productsDisplay(products){
 // Regex, replace(/\w\.jpg/gi, 'W.jpg')
 
 // Função para o gráfico
-
 function updatePriceChart(products){
-    const ctx = priceChart.getContext('2d')
-    
+    const ctx = priceChart.getContext('2d');
+    if(myChart){
+        myChart.destroy();
+    }
+
+    myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: products.map(product => product.title.substring(0, 20) + '...'),
+            datasets: [{
+                label: 'Preço (R$)',
+                data: products.map(product => product.price),
+                backgroundColor: 'rgba(46, 204, 113, 0.6)', 
+                borderColor: 'rgba(46, 204, 113, 1)',
+                borderWidth: 1
+            }]
+        }, 
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true, 
+                    ticks: {
+                        callback: function(value){
+                            return (
+                                'R$' + value.toLocaleString('pt-br', {
+                                    style: "currency",
+                                    currency: "BRL",
+                                })
+                            )
+                        },
+                    }, 
+                    plugins: {
+                        lagend: {
+                            display: false
+                        },
+                        title: {
+                            display: true,
+                            text: 'Comparador de Preços',
+                            font: {
+                                size: 18
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    })
 }
 
 
